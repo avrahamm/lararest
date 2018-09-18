4ba3b9220a062ba1427d8ddf1795467effa40cd2
avrahamm <papaabramg@gmail.com>
2018-09-18 12:42:46 +0300

    p5-DB before authentication

--- app/User.php
+++ app/User.php
@@ -2,14 +2,10 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -27,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function meetings() {
+        return $this->belongsToMany('App\Meeting');
+    }
 }
